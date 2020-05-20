b3ddc9ebabf725020c84f0b52f2ad78af706eefd
cnjones7 <>
2020-05-20 11:45:50 -0500

    Hello World

---
+++ R/waterLevel_demo.R
@@ -0,0 +1,31 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Title: Water Level Demo
+#Coder: Nate Jones
+#Date: 5/20/2020
+#Purpose: Demo Water Level Processing
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Step 1: Setup workspace--------------------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Clear memory
+remove(list=ls())
+
+#install relevant packages
+# install.packages('tidyverse')
+# install.packages('xts')
+# install.packages('dygraph')
+# install.packages("htmlwidgets")
+
+#Load libraries of interest
+library(tidyverse)
+library(xts)
+library(dygraphs)
+library(htmlwidgets)
+
+#Download data
+wetland<-read_csv('data/wetland.csv')
+baro<-read_csv('data/baro.csv')
+
+
+
